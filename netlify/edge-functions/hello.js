a746d594a02c760f530fd9616c2885f6e85f2bc2
GoldenAngel <50855202+GoldenAngel2@users.noreply.github.com>
2023-07-11 05:03:46 -0700

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,11 @@
-export default async (request) => {
-  return new Response("Hello, World!", {
-    headers: { "content-type": "text/html" },
+import { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+  const joke = await fetch("https://my.elara.services/site/ping", {
+    "headers": {
+      "Accept": "application/json"
+    }
   });
+  const jsonData = await joke.json();
+  return Response.json(jsonData);
 };
