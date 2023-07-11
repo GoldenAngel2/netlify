a179c81b82104ca394257be6788b18192b7a77b8
GoldenAngel <50855202+GoldenAngel2@users.noreply.github.com>
2023-07-11 05:35:59 -0700

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,9 @@
 export default async (request, context) => {
-  const res = await fetch(`https://haste.discordcatto.repl.co/documents/${new URL(request.url).searchParams.get("id")}`, {
+  const id = new URL(request.url).searchParams.get("id");
+  if (!id || typeof id !== "string") {
+    return Response.json({ status: false, message: `You didn't provide the 'id' query.` });
+  }
+  const res = await fetch(`https://haste.discordcatto.repl.co/documents/${id}`, {
     "headers": {
       "Accept": "application/json"
     }
