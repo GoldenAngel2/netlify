0f5904d5a46765f6a8c0c63772ede4ba3033afbd
GoldenAngel <50855202+GoldenAngel2@users.noreply.github.com>
2023-07-13 04:14:48 -0700

    Update json.ts

--- netlify/edge-functions/json.ts
+++ netlify/edge-functions/json.ts
@@ -1,5 +1,16 @@
-import type { Context } from "https://edge.netlify.com";
-
-export default async (request: Request, context: Context) => {
-  return Response.json({ hello: "world" });
+export default async (request, context) => {
+  const url = new URL(request.url);
+  const uri = url.searchParams.get("url");
+  if (!uri) {
+    return Response.json({ status: false, message: `You failed to provide a url.` });
+  };
+  const res = await fetch(decodeURIComponent(uri), {
+    method: request.method,
+    body: request.body,
+    headers: request.headers,
+  }).catch(e => e);
+  if (res.status !== 200) {
+    return Response.json({ status: false, message: `No response from the API` });
+  }
+  return Response.json(await res.json());
 };
