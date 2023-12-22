31ed7e505ed7a5d1704104c33c1983d84aea6aa2
GoldenAngel <50855202+GoldenAngel2@users.noreply.github.com>
2023-12-21 19:20:32 -0800

    Update json.ts

--- netlify/edge-functions/json.ts
+++ netlify/edge-functions/json.ts
@@ -9,7 +9,6 @@ export default async (request, context) => {
     body: request.body,
     headers: request.headers,
   }).catch(e => e);
-  console.log(res, await res.text());
   if (res.status !== 200) {
     return Response.json({ status: false, message: `No response from the API` });
   }
